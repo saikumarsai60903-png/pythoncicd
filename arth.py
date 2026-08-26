320f4d86dc2530efa9fc10e2a0c5d731329539c8
Saikumar <saikumarsai60903@gmail.com>
2026-08-27 00:25:17 +0530

    python

---
+++ arth.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b)
+    return a*b 
+if__name__=="__main__":
+    print("addition is",add(10,5))
+    print("subtraction is",sub(10,5))
+    print("multiplication is",mul(10,5))
