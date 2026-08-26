8f6fa66acd9245c6edb5d823c30a5849f4738f1c
saikumarsai60903-png <saikumarsai60903@gmail.com>
2026-08-27 01:51:20 +0530

    Update arthtest.py

--- arthtest.py
+++ arthtest.py
@@ -1,11 +1,18 @@
- from arth import *
 import unittest
-class ArthTest(unittest.TestCase):
- def test_add(self):
-    self.assertEqual(add(10,5),15)
-def test_sub(self):
-    self.assertEqual(sub(10,5),5)
-def test_mul(self):
-    self.assertEqual(mul(10,5),50)
-if__name__=="__main__"
-   unittest.main()
+from arth import add, sub, mul
+
+
+class TestArth(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(10, 5), 15)
+
+    def test_sub(self):
+        self.assertEqual(sub(10, 5), 5)
+
+    def test_mul(self):
+        self.assertEqual(mul(10, 5), 50)
+
+
+if __name__ == "__main__":
+    unittest.main()
